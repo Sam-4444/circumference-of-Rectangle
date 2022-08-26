d54340a7fd4604eb67dd594b6953e280a647033c
Sam <106349608+Sam-4444@users.noreply.github.com>
2022-08-26 19:50:31 +0430

    Update circumferenceOfRectangle.py

--- circumferenceOfRectangle.py
+++ circumferenceOfRectangle.py
@@ -1,10 +1,15 @@
-#1. taking inputs
+#1. taking inputs and calculate circumference Of Rectangle 
+
+
 base=input("enter the base amount and press enter !")
 height=input("enter the height amount and press enter !" )
 print("length of height :"+str(int(height)))
 print("length of base :"+str(int(base)))
 print("the circumference is : "+str((int(base)*int(height))))
+
+
 #2. as a function
+
 def circunference(base,height):
   print("length of height :" + str(int(height)))
   print("length of base :" + str(int(base)))
